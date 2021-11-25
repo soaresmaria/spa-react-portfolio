79fdfab88aa56b5bc7e37c7ca975f799dc276254
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-25 01:16:25 -0500

    added project to data

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 export default
     {
-        
-        name :'Maria Soares',
-        headerTagline: [
-                        'I am Maria Soares,',
-                        'a full stack web developer',
-                        'from Edison.'
-    ],
-        contactEmail:'soaresmaria181@yahoo.com',
-        resume: "./Maria Resume.pdf",
-        abouttext: "I am a well organized people person who is focused on excellent client support, program development, and complex problem solving. Passionate about helping others. Cultivate positive morale by coaching and encouraging others.",
-        aboutImage:'./profilePic.jpg',
-        ShowAboutImage:true,
 
-        projects:[
+        name: 'Maria Soares',
+        headerTagline: [
+            'I am Maria Soares,',
+            'a full stack web developer',
+            'from Edison.'
+        ],
+        contactEmail: 'soaresmaria181@yahoo.com',
+        resume: "./Maria-Resume.pdf",
+        abouttext: "I am a well organized people person who is focused on excellent client support, program development, and complex problem solving. Passionate about helping others. Cultivate positive morale by coaching and encouraging others.",
+        aboutImage: './profile-pic.jpg',
+        ShowAboutImage: true,
+
+        projects: [
             {
                 id: 1,
                 title: 'Taskmaster-pro',
@@ -28,52 +28,62 @@ export default
                 imageSrc: "./web-api.png",
                 url: 'https://soaresmaria.github.io/web-api-quiz/'
             },
-    
-               {
+
+            {
                 id: 3,
-                title:'Password Generator', 
-                 service:'First use of javascript', 
-                 imageSrc:"./password-gen.png",
-                 url:'https://soaresmaria.github.io/password-gen/'
-                },
-                {
-                    id: 4,
-                    title: 'Budget Tracker',
-                    service: 'Application that handles users’ financial information.',
-                    imageSrc: "./budget.png",
-                    url: 'https://financial-estimate.herokuapp.com/'
-                },
-                { 
-                    id: 5,
-                    title: 'Professional Portfolio',
-                    service: 'My Professional Portfolio',
-                    imageSrc: "./porfolio.png",
-                    url: 'https://soaresmaria.github.io/professional-portfolio/#'
-                },
-                {
-                    id: 6,
-                    title: 'Calendar App',
-                    service: 'A simple calendar app for scheduling your work day',
-                    imageSrc: "./scheduler.png",
-                    url: 'https://soaresmaria.github.io/third-party-api/'
-               }
-    
-                            
-                    
-                   
-            ],
-            social: [
-    
-                {   name:'Github',
-                    url:'https://github.com/soaresmaria'},
-                {
-                    name: 'Linkedin',
-                    url: 'https://www.linkedin.com/in/maria-soares-26941820b'
-                },
-                {
-                    name: 'Instagram',
-                    url: 'https://www.instagram.com/mariso1220/'
-                }
-    
-            ]
-        }
+                title: 'Password Generator',
+                service: 'First use of javascript',
+                imageSrc: "./password-gen.png",
+                url: 'https://soaresmaria.github.io/password-gen/'
+            },
+            {
+                id: 4,
+                title: 'Budget Tracker',
+                service: 'Application that handles users’ financial information.',
+                imageSrc: "./budget.png",
+                url: 'https://financial-estimate.herokuapp.com/'
+            },
+            {
+                id: 5,
+                title: 'Professional Portfolio',
+                service: 'My Professional Portfolio',
+                imageSrc: "./porfolio.png",
+                url: 'https://soaresmaria.github.io/professional-portfolio/#'
+            },
+            {
+                id: 6,
+                title: 'Calendar App',
+                service: 'A simple calendar app for scheduling your work day',
+                imageSrc: "./scheduler.png",
+                url: 'https://soaresmaria.github.io/third-party-api/'
+            },
+            {
+                id: 7,
+                title: 'Global-Trade',
+                service: 'A regional shopping app',
+                imageSrc: "./global-trade.png",
+                url: 'https://still-eyrie-57534.herokuapp.com/'
+
+            }
+
+
+
+
+        ],
+        social: [
+
+            {
+                name: 'Github',
+                url: 'https://github.com/soaresmaria'
+            },
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/maria-soares-26941820b'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/mariso1220/'
+            }
+
+        ]
+    }
